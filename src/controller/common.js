1fa52881b71e7808f15b5c169ab37de6e8e6f24b
Doooyf <454048833@qq.com>
2018-09-05 10:17:36 +0800

    登录登出

--- src/controller/common.js
+++ src/controller/common.js
@@ -4,38 +4,42 @@
  @Author：贤心
  @Site：http://www.layui.com/admin/
  @License：LPPL
-    
- */
- 
-layui.define(function(exports){
-  var $ = layui.$
-  ,layer = layui.layer
-  ,laytpl = layui.laytpl
-  ,setter = layui.setter
-  ,view = layui.view
-  ,admin = layui.admin
-  
-  //公共业务的逻辑处理可以写在此处，切换任何页面都会执行
-  //……
-  
-  
-  
-  //退出
-  admin.events.logout = function(){
-    //执行退出接口
-    admin.req({
-      url: './json/user/logout.js'
-      ,type: 'get'
-      ,data: {}
-      ,done: function(res){ //这里要说明一下：done 是只有 response 的 code 正常才会执行。而 succese 则是只要 http 为 200 就会执行
-        
-        //清空本地记录的 token，并跳转到登入页
-        admin.exit();
-      }
-    });
-  };
 
-  
-  //对外暴露的接口
-  exports('common', {});
+ */
+
+layui.define(function (exports) {
+    var $ = layui.$
+        , layer = layui.layer
+        , laytpl = layui.laytpl
+        , setter = layui.setter
+        , view = layui.view
+        , admin = layui.admin
+
+    //公共业务的逻辑处理可以写在此处，切换任何页面都会执行
+    var token = JSON.parse(localStorage.getItem('layuiAdmin'))['access_token'];
+    //……
+
+
+    //退出
+    admin.events.logout = function () {
+        //执行退出接口
+        admin.req({
+            url: layui.api + 'agent/api/logout'
+            , type: 'get'
+            , headers: {'Authorization': 'Bearer ' + token}
+            , data: {}
+            // , done: function (res) { //这里要说明一下：done 是只有 response 的 code 正常才会执行。而 succese 则是只要 http 为 200 就会执行
+            //
+            //     //清空本地记录的 token，并跳转到登入页
+            //     admin.exit();
+            // }
+            , success: function (data) {
+                admin.exit();
+            }
+        });
+    };
+
+
+    //对外暴露的接口
+    exports('common', {});
 });
